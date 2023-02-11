9237360bd6f2ec6301ac2cacea2f88a857bc0bd8
Igoryan <nigmatullin.igor2014@yandex.ru>
2023-02-11 12:45:02 +0300

    добавил комментарий

--- Order111/Классы/Order.cs
+++ Order111/Классы/Order.cs
@@ -12,7 +12,7 @@ namespace OrderGen.Domain
     {   
         private readonly IList<OrderItem> _orderItems = new List<OrderItem>();
         private int _id;//Номер накладной
-        public Order()//Конструктор 
+        public Order()///Конструктор 
         {
             _id = 1; //Номер накладной начинается с 1
             Created = DateTime.Today;//Запоминаем сегодняшнюю дату
